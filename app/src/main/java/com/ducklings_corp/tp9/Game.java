0a5322aacb2cdcf5620d95f4141c8b5b7071e4a7
Android Studio <devandroid@ort>
2019-11-05 10:23:33 -0300

    enemies and colition

--- app/src/main/java/com/ducklings_corp/tp9/Game.java
+++ app/src/main/java/com/ducklings_corp/tp9/Game.java
@@ -1,6 +1,7 @@
 package com.ducklings_corp.tp9;
 
 import android.util.Log;
+import android.view.MotionEvent;
 
 import org.cocos2d.actions.instant.CallFuncN;
 import org.cocos2d.actions.interval.IntervalAction;
@@ -14,7 +15,6 @@ import org.cocos2d.nodes.Director;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
-import org.cocos2d.types.CCPoint;
 import org.cocos2d.types.CCSize;
 
 import java.util.ArrayList;
@@ -34,6 +34,7 @@ public class Game {
         Director.sharedDirector().attachInView(this._gameView);
         Scene scene = startScene();
         Director.sharedDirector().runWithScene(scene);
+
     }
 
     private Scene startScene() {
@@ -42,11 +43,12 @@ public class Game {
 
         MainLayer mainLayer = new MainLayer(_screen);
         returnScene.addChild(mainLayer, -1);
-        PlayerLayer playerLayer = new PlayerLayer(_screen);
-        returnScene.addChild(playerLayer, 1);
+        EntitiesLayer entitiesLayer = new EntitiesLayer(_screen);
+        returnScene.addChild(entitiesLayer, 1);
 
         return returnScene;
     }
+
 
 
 }
@@ -59,7 +61,7 @@ class MainLayer extends Layer {
         _screen = screen;
         createBg(0);
         createBg(1);
-        super.schedule("backgroundGen",SCROLL_SPEED/2);
+        super.schedule("backgroundGen", SCROLL_SPEED / 2);
     }
 
     public void backgroundGen(float dt) {
@@ -67,41 +69,102 @@ class MainLayer extends Layer {
     }
 
     public void endScroll(CocosNode node) {
-        super.removeChild(node,true);
+        super.removeChild(node, true);
     }
 
     public void createBg(int i) {
-            Sprite bg = Sprite.sprite("background.png");
-            bg.setPosition(_screen.getWidth() / 2, _screen.getHeight() / 2 + _screen.getHeight() * i);
+        Sprite bg = Sprite.sprite("background.png");
+        bg.setPosition(_screen.getWidth() / 2, _screen.getHeight() / 2 + _screen.getHeight() * i);
 
-            float fWidth, fHeight;
-            fWidth = _screen.getWidth() / bg.getWidth();
-            fHeight = _screen.getHeight() / bg.getHeight();
-            Log.d("MainLayer", String.format("sx %s sy %s", fWidth, fHeight));
-            bg.runAction(ScaleBy.action(0.0000001f, fWidth, fHeight));
+        float fWidth, fHeight;
+        fWidth = _screen.getWidth() / bg.getWidth();
+        fHeight = _screen.getHeight() / bg.getHeight();
+        Log.d("MainLayer", String.format("sx %s sy %s", fWidth, fHeight));
+        bg.setScaleX(fWidth);
+        bg.setScaleY(fHeight);
 
 
-            CallFuncN sequenceCall = CallFuncN.action(this, "endScroll");
-            IntervalAction scroll = Sequence.actions(MoveBy.action(SCROLL_SPEED, 0, -_screen.height*2), sequenceCall);
-            bg.runAction(scroll);
-            super.addChild(bg);
+        CallFuncN sequenceCall = CallFuncN.action(this, "endScroll");
+        IntervalAction scroll = Sequence.actions(MoveBy.action(SCROLL_SPEED, 0, -_screen.height * 2), sequenceCall);
+        bg.runAction(scroll);
+        super.addChild(bg);
 
     }
 }
 
-class PlayerLayer extends Layer {
+class EntitiesLayer extends Layer {
     Sprite _player;
+    CCSize _screen;
 
-    public PlayerLayer(CCSize screen) {
+    final int ENEMY_PY = 40;
+    final int PLAYER_PY = 37;
+
+    public ArrayList<Sprite> _enemies = new ArrayList();
+
+    public EntitiesLayer(CCSize screen) {
+        _screen = screen;
         _player = Sprite.sprite("player.png");
         _player.setPosition(screen.getWidth() / 2, screen.getHeight() / 2);
         _player.runAction(ScaleBy.action(0.1f, 3, 3));
         super.addChild(_player);
-    }
-}
+        setIsTouchEnabled(true);
 
-class EnemyLayer extends Layer {
-    public EnemyLayer() {
-
+        super.schedule("spawnSmall",3);
+        super.schedule("detectCol",0.25f);
     }
+    public void detectCol(float dt) {
+        for(CocosNode enemy: _enemies) {
+            float dist = pythagoras(enemy,_player);
+            if(dist < ENEMY_PY + PLAYER_PY) {
+                Log.d("EntitiesLayer", "LOL");
+            }
+        }
+    }
+    public float pythagoras(CocosNode a,CocosNode b) {
+        float dx = Math.abs(a.getPositionX() - b.getPositionX());
+        float dy = Math.abs(a.getPositionY() - b.getPositionY());
+        return (float)Math.sqrt(dx*dx + dy*dy);
+    }
+
+    public boolean intersectionWithSprites(Sprite Sprite1, Sprite Sprite2) {
+        Boolean intersection = false;
+
+        return intersection;
+    }
+
+    @Override
+    public boolean ccTouchesMoved(MotionEvent event) {
+        _player.setPosition(event.getX(), this.getHeight() - event.getY());
+
+        return super.ccTouchesMoved(event);
+    }
+
+    public void spawnSmall(float dt) {
+        Sprite sprite = Sprite.sprite("enemy.png");
+        sprite.setPosition((float) Math.random() * _screen.getWidth(), _screen.getHeight());
+        sprite.runAction(ScaleBy.action(0.1f, 3, 3));
+
+        float xtarget_1, xtarget_2;
+        if (sprite.getPositionX() > _screen.getWidth() / 2) {
+            xtarget_1 = 0;
+        } else {
+            xtarget_1 = _screen.getWidth();
+        }
+
+        xtarget_2 = _screen.getWidth() - xtarget_1;
+
+        IntervalAction action = Sequence.actions(
+                MoveTo.action(1, xtarget_1, _screen.getHeight() / 4 * 3),
+                MoveTo.action(2, xtarget_2, _screen.getHeight() / 4 * 2),
+                MoveTo.action(2, xtarget_1, _screen.getHeight() / 4 * 1),
+                MoveTo.action(2, xtarget_2, 0),
+                MoveBy.action(0.5f, 0, -150)/*,
+                CallFuncN.action(this, "endMyLife")*/
+        );
+
+        sprite.runAction(action);
+        _enemies.add(sprite);
+        super.addChild(sprite);
+    }
+
 }
